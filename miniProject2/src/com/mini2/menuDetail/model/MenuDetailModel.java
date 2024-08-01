5c9fe6b275539cbc0aef61cb5014b5da12939dc0
MoonMinah <mingke48@gmail.com>
2024-08-02 01:38:34 +0900

    Update MenuDetailModel.java

--- miniProject2/src/com/mini2/menuDetail/model/MenuDetailModel.java
+++ miniProject2/src/com/mini2/menuDetail/model/MenuDetailModel.java
@@ -1,5 +1,9 @@
 package com.mini2.menuDetail.model;
 
 public class MenuDetailModel {
+    private int menu_detail_id;
+    private int item_id;
+    private int order_id;
+    private int quantity;
 
 }
