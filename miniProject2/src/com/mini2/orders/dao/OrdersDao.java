e3339f99aa2ef51e23c1007f7fa0a2eae545d5e9
MoonMinah <mingke48@gmail.com>
2024-08-02 09:17:10 +0900

    Update OrdersDao.java

--- miniProject2/src/com/mini2/orders/dao/OrdersDao.java
+++ miniProject2/src/com/mini2/orders/dao/OrdersDao.java
@@ -1,5 +1,9 @@
 package com.mini2.orders.dao;
 
 public class OrdersDao {
-
+    private int order_id;
+  	private int user_id;
+	  private Date order_date;
+	  private int total_amount;
+	  private boolean status;
 }
