573b2f44bb765d2d532e33fd97b4953992af56f2
Lee Haeun <kmbalhae07@naver.com>
2024-08-02 10:45:59 +0900

    add regular expression

--- miniProject2/src/com/mini2/users/controller/UsersController.java
+++ miniProject2/src/com/mini2/users/controller/UsersController.java
@@ -3,6 +3,9 @@ package com.mini2.users.controller;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.mini2.users.model.UserInsert;
 import com.mini2.users.model.UserLogin;
 import com.mini2.users.model.UserUpdate;
@@ -15,35 +18,68 @@ public class UsersController {
     private UserDelete userDelete = new UserDelete();
     private Scanner sc = new Scanner(System.in);
     private Map<String, Integer> session = new HashMap<>();
-    String regex = "[!@#$%^&*]";
 
     //사용자 등록을 처리
     public void registerUser() {
         boolean check = true;
-        String password = null;
+        String password = null, phoneNumber = null, email = null;
         System.out.println("회원가입을 위해 다음 정보를 입력하세요.");
         System.out.print("이름: ");
         String userName = sc.nextLine();
 
-        System.out.print("이메일: ");
-        String email = sc.nextLine();
+        // 이메일 검증 정규식
+     	String regexrEmail = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+     	while (check) {
+     		System.out.print("이메일: ");
+     		email = sc.nextLine();
+     		
+     		Pattern pattern = Pattern.compile(regexrEmail);
+			Matcher matcher = pattern.matcher(email);
 
+			if (!matcher.matches()) {
+				System.out.println("올바르지 않은 이메일 양식입니다.");
+			} else {
+				check = false;
+			}
+     	}
+     	check = true;
+     	
         //비밀번호검증 -> 최소 8글자, 특수문자 포함
+     	String regexrPassword = "[!@#$%^&*]";
         while (check) {
             System.out.println("※주의! 비밀번호는 최소 8글자와 특수문자가 포함되어야 합니다.\n");
             System.out.print("비밀번호: ");
             password = sc.nextLine();
-g
-            if (password.length() < 8 || !password.matches(".*[!@#$%^&*].*")) {
-                System.out.println("비밀번호는 8글자 이상이며, 최소 하나의 특수문자가 포함되어야 합니다.\n");
-            } else {
-                check = false; // 비밀번호 검증을 통과한 경우 루프 종료
-            }
+
+            if (password.length() < 8) {
+				System.out.println("비밀번호는 8글자 이상이어야 합니다.\n");
+			} else {
+				Pattern pattern = Pattern.compile(regexrPassword);
+				Matcher matcher = pattern.matcher(password);
+				if (!matcher.find()) {
+					System.out.println("비밀번호는 최소 하나의 특수문자가 포함되어야 합니다.\n");
+				} else {
+					check = false; // 비밀번호 검증을 통과한 경우 루프 종료
+				}
+			}
         }
 
-        System.out.print("전화번호: ");
-        String phoneNumber = sc.nextLine();
+        // 전화번호 검증 정규식
+     	String regexrPhoneNumber = "^\\d{3}-\\d{3,4}-\\d{4}$";
+     	while (check) {
+     		System.out.print("전화번호: ");
+        	phoneNumber = sc.nextLine();
+        	
+        	Pattern pattern = Pattern.compile(regexrPhoneNumber);
+			Matcher matcher = pattern.matcher(phoneNumber);
 
+			if (!matcher.matches()) {
+				System.out.println("올바르지 않은 전화번호 양식입니다.");
+			} else {
+				check = false;
+			}
+     	}
+     	
         System.out.print("주소: ");
         String address = sc.nextLine();
 
