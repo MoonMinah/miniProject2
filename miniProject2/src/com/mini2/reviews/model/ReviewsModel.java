6cb8b60e997e7be0d0e600107b4ddddd0c84c158
unknown <jiyeon979797@gmail.com>
2024-08-02 19:12:59 +0900

    리뷰 작업중

---
+++ miniProject2/src/com/mini2/reviews/model/ReviewsModel.java
@@ -0,0 +1,15 @@
+package com.mini2.reviews.model;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class ReviewsModel {
+	private int reviewId; //리뷰 아이디
+	private int paymentId; //결제 아이디
+	private int rating; //평점
+	private String comment; //리뷰 내용
+	private Timestamp reviewDate; //리뷰 날짜
+	
+}
